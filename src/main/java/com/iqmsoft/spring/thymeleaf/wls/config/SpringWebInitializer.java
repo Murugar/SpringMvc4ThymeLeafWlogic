ef1e78cf18a6ecfee10fbf0bbb9714c8e7ca5042
Davanon <davanon2014@gmail.com>
2017-03-23 15:57:03 +0530

    first

---
+++ src/main/java/com/iqmsoft/spring/thymeleaf/wls/config/SpringWebInitializer.java
@@ -0,0 +1,28 @@
+package com.iqmsoft.spring.thymeleaf.wls.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * Spring configuration and Spring MVC bootstrapping.
+ */
+@Configuration
+public class SpringWebInitializer implements WebApplicationInitializer {
+
+    
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(ThymeleafConfig.class);
+        context.setServletContext(servletContext);
+        // Spring MVC front controller
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/java/com/iqmsoft/spring/thymeleaf/wls/web/IndexController.java
@@ -0,0 +1,25 @@
+package com.iqmsoft.spring.thymeleaf.wls.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("recipient", "Welcome Peter");
+        model.addAttribute("message", "This is a Simple Test");
+        return "index.html";
+    }
+    
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String admin(Model model) {
+        model.addAttribute("recipient", "Welcome Peter");
+        model.addAttribute("message", "This is a Simple Test");
+        return "admin.html";
+    }
+    
+}
